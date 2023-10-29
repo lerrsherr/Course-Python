ce800caa23246b6d239c720985e7f0728d0cabe4
lerrsherr <149315129+lerrsherr@users.noreply.github.com>
2023-10-29 19:47:36 +0300

    Add files via upload

---
+++ Лаборат.раб.1/t3.py
@@ -0,0 +1,11 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+tot = len(list_players)
+
+nkom1 = round(tot/2)
+
+komanda1 = list_players[:nkom1]
+komanda2 = list_players[nkom1:]
+print(komanda1)
+print(komanda2)
+# TODO Разделите участников на две команды
