954bdb323e79b06415a052842ba34323326008f5
lerrsherr <149315129+lerrsherr@users.noreply.github.com>
2023-11-01 12:14:48 +0300

    Update t1.py

--- Лаборат.раб.1/t1.py
+++ Лаборат.раб.1/t1.py
@@ -3,7 +3,7 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 # TODO заменить значение пропущенного элемента средним арифметическим
 count = len(numbers)
 numbers_none = numbers.index(None)
-sum_numbers = sum(numbers[:4] + numbers[4+1:])
+sum_numbers = sum(numbers[:numbers_none] + numbers[numbers_none+1:])
 average = sum_numbers/count
 
 numbers[4] = average
